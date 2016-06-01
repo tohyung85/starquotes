5a111986c3d72043af71f291e1798b435ef757cd
Your Name <tantohyung@gmail.com>
2016-06-01 09:21:51 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starquotes::Application.config.secret_key_base = '15442bbe3b90bb1d7082a3dccc69cea953cb4be88aaa0ea7915e08bf563e84aa1d039422af91740c1fa7199a50f60db3eddedb74d33d3e42b3e1d670aa1a2eeb'
