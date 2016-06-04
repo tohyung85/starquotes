f6de6e4a424ceabeabba04833e127a50e479559d
Your Name <tantohyung@gmail.com>
2016-06-04 02:50:15 +0000

    Form validation done

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ActiveRecord::Base
+  validates :saying, presence: true, length: {maximum: 500, minimum: 3}
+  validates :author, presence: true, length: {maximum: 200, minimum: 3}
+  validates :poster_image, presence: true, length: {minimum: 3}
 end
